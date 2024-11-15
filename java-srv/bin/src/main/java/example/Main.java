3fb262cac34a36bd8364f700720b0c606b013935
bearddan2000 <bearddan2000@yahoo.com>
2024-11-15 11:52:26 -0500

    initial commit

--- java-srv/bin/src/main/java/example/Main.java
+++ java-srv/bin/src/main/java/example/Main.java
@@ -7,8 +7,6 @@ import com.blade.mvc.http.Session;
 
 public class Main {
 
-    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(Main.class);
-
     public static void main(String[] args) {
 
         Blade.of()
